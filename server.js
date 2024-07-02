0e71594c186e967bdb80b4bdfa2b050c4b1868ae
erickystn <xkyx@live.com>
2024-07-01 21:12:36 -0300

    css tst vercel

--- server.js
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(exports, '__esModule', { value: true }); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } const _app = require('./dist/app');
-
-const _app2 = _interopRequireDefault(_app);
-
-_app2.default.listen(3000, () => console.log('iniciou!'));
-
-// Export the Express API para funcionar no VERCEL
-exports.default = _app2.default;
