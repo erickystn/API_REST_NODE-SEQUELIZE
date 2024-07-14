79b3b851f289eed0240519e5d64163fbc73bc11d
erickystn <xkyx@live.com>
2024-07-13 22:24:36 -0300

    add cors and helmet

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 import dotenv from 'dotenv';
 import { resolve } from 'path';
+import cors from 'cors';
 
 dotenv.config();
 
 import './database';
 
 import express from 'express';
+import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import photoRoutes from './routes/photoRoutes';
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,6 +33,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(helmet());
+    this.app.use(cors(corsOptions));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, '..', 'static')));
