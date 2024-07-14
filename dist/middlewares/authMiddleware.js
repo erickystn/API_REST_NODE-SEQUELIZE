e606b96f973013968262fb8ccf4130beab870dbc
erickystn <xkyx@live.com>
2024-07-14 12:12:55 -0300

    builded file

--- dist/middlewares/authMiddleware.js
+++ dist/middlewares/authMiddleware.js
@@ -5,23 +5,24 @@ exports. default = async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.status(401).json({ errors: ['Token not provided'] });
+    return res.status(401).json({ errors: ['Token não fornecido'] });
   }
 
   const [, token] = authorization.split(' ');
 
   try {
     const { id, email } = _jsonwebtoken2.default.verify(token, process.env.TOKEN_SECRET);
+    console.log('aquiii', _jsonwebtoken2.default.verify(token, process.env.TOKEN_SECRET));
 
     const user = await _User2.default.findOne({ where: { id, email } });
     if (!user) {
-      return res.status(401).json({ errors: ['User invalid'] });
+      return res.status(401).json({ errors: ['Usuário Inválido'] });
     }
 
     req.userId = id;
     req.userEmail = email;
     return next();
   } catch (e) {
-    return res.status(401).json({ errors: ['Token invalid or expired'] });
+    return res.status(401).json({ errors: ['Token Inválido ou expirado'] });
   }
 };
